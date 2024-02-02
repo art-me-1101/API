5bd5786384881f96b5100a9c84026872f312bb93
name <dimannov99@gmail.com>
2024-02-02 10:06:55 -0500

    punkt 2

--- main1/main.py
+++ main1/main.py
@@ -36,10 +36,24 @@ def draw_map():
     b = a.get_rect()
     screen = pygame.display.set_mode((b.w, b.h))
     screen.blit(pygame.image.load(map_file), (0, 0))
-
-    pygame.display.flip()
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.K_UP:
+                print(2)
+                if z > 1:
+                    z -= 1
+                    new_search(x, y, z, tipe)
+                    a = pygame.image.load(map_file)
+            elif event.type == pygame.K_DOWN:
+                if z < 20:
+                    z += 1
+                    new_search(x, y, z, tipe)
+                    a = pygame.image.load(map_file)
+        screen.blit(a, (0, 0))
+        pygame.display.flip()
     pygame.quit()
     os.remove(map_file)
 
