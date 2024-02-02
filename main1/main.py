1352595596ac2da4e2953247d6c7ccf11d86ee41
name <dimannov99@gmail.com>
2024-02-02 09:41:41 -0500

    punkt 1

--- main1/main.py
+++ main1/main.py
@@ -1,11 +1,42 @@
+import os
+import sys
+import pygame
+import requests
+
+map_file = 'map.png'
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def new_search(coordinate, z, tipe):
+    x, y = map(float, coordinate.split())
+    map_request = f"http://static-maps.yandex.ru/1.x/?ll={x},{y}&z={z}&l={tipe}"
+    response = requests.get(map_request)
 
+    if not response:
+        sys.exit(1)
+
+    global map_file
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+
+
+def user_input():
+    return input(), int(input()), input()
+
+
+def draw_map():
+    pygame.init()
+    a = pygame.image.load(map_file)
+    b = a.get_rect()
+    screen = pygame.display.set_mode((b.w, b.h))
+    screen.blit(pygame.image.load(map_file), (0, 0))
+
+    pygame.display.flip()
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
+    pygame.quit()
+    os.remove(map_file)
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    new_search(*user_input())
+    draw_map()
